8711555399afdc2b8f0019989c39462ebcdc0729
GEORGE MAKRIS <georgemakris@GEORGEs-MacBook-Air.local>
2017-12-13 20:04:48 -0600

    First Blog

--- georgesblog/urls.py
+++ georgesblog/urls.py
@@ -20,6 +20,7 @@ import posts.views
 import sitepages.views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,5 +28,7 @@ urlpatterns = [
     url(r'^posts/(?P<post_id>[0-9]+)/$', posts.views.post_details, name='post_details'),
     url(r'^about/$', sitepages.views.about, name='about'),
     url(r'^contact/$', sitepages.views.contact, name='contact'),
-    
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
